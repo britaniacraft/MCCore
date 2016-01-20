c0b5a7025fcca007c4d48cab3853f6df7bbb6de0
Falistos <falistos@minetop.fr>
2016-01-20 03:42:07 +0100

    Removed unused import

--- src/com/rit/sucy/config/parse/DataSection.java
+++ src/com/rit/sucy/config/parse/DataSection.java
@@ -1,10 +1,18 @@
 package com.rit.sucy.config.parse;
 
-import com.sun.xml.internal.fastinfoset.Encoder;
-import org.bukkit.Bukkit;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-import java.io.*;
-import java.util.*;
+import com.sun.xml.internal.fastinfoset.Encoder;
 
 /**
  * Represents a section of a config
